98d0995e76a238d71cf470aeb22d56a994d386bf
Oisin Cousins <oisincousins@gmail.com>
2026-08-11 22:33:57 +0100

    config: sync insertions(+)

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters_by_ft = {
+        rust = { "rustfmt", lsp_format = "fallback" },
+      },
+    },
+  },
+  {
     "mrcjkb/rustaceanvim",
     opts = function(_, opts)
       local ra = opts.server.default_settings["rust-analyzer"]
